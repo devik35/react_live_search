47aa0dd56f79d01d6382e4a6e430505eae35d7df
Washington-Kimani <101510408+Washington-Kimani@users.noreply.github.com>
2024-02-17 11:23:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react_live_search
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Washington-Kimani/react_live_search)
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useMemo, useRef } from 'react';
+import './style.css';
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  const [query, setQuery] = useState("");
+  const inputRef = useRef();
+
+  const filteredItems = useMemo(()=>{
+    return items.filter(item=>{
+    return item.toLowerCase().includes(query.toLowerCase())
+  })
+},[query, items])
+
+  function onSubmit(e){
+    e.preventDefault();
+
+    const value = inputRef.current.value;
+    if(value === "") return;
+    setItems(prev =>{
+      return[...prev, value]
+    })
+    inputRef.current.value = '';
+  };
+  return (
+    <div>
+      Search
+      <input type="search" value={query} onChange={e=>setQuery(e.target.value)} />
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        New Item:
+        <input ref={inputRef} type="text" name="text" id="text" />
+        <button type="submit">Add</button>
+        <br />
+      </form>
+      <h3>Items:</h3>
+      {
+        filteredItems.map(item=>(
+          <div>{item}</div>
+        ))
+      }
+    </div>
+  );
+}
